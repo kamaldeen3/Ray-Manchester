f6678504dfb16fc6fefa611ba8e3c506ffa1bfac
Kamaldeen Shittu <575749@live.oldham.ac.uk>
2026-03-07 09:58:47 +0000

    vv

--- sin/anyapp/admin.py
+++ sin/anyapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at', 'updated_at']
+    search_fields = ['user__username', 'user__email', 'user__first_name', 'user__last_name']
+    list_filter = ['created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+    
+admin.site.register(UserProfile, UserProfileAdmin)
+
+
